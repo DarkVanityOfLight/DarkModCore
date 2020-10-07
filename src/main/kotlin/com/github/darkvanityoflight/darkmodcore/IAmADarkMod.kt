e8170d9b01de09a0aecfeb552ae45af1aff3c8a1
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-07 14:09:44 +0200

    feat: Implement interface for IAmADarkMod

--- src/main/kotlin/com/github/darkvanityoflight/darkmodcore/IAmADarkMod.kt
+++ src/main/kotlin/com/github/darkvanityoflight/darkmodcore/IAmADarkMod.kt
@@ -1,5 +1,27 @@
 package com.github.darkvanityoflight.darkmodcore
 
+import com.github.darkvanityoflight.darkmodcore.configparser.IParse
+
 
 interface IAmADarkMod {
+    var configParser : IParse?
+
+    companion object{
+        lateinit var instance : IAmADarkMod
+    }
+
+    fun debug(message : String?)
+    fun debug(message: String?, vararg vars: Any?)
+
+    fun severe(message: String?)
+    fun severe(message: String?, error : Throwable?)
+
+    fun warning(message: String?)
+    fun warning(message: String?, error: Throwable?)
+    fun warning(message: String?, error: Throwable?, vararg vars: Any?)
+
+    fun info(message: String?)
+    fun info(message: String?, vararg vars: Any?)
+
+
 }
