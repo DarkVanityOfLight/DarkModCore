af37f46d8c5072fb4df9d7d5dd59a92314fc2d1f
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-07 14:53:18 +0200

    feat: override onEnable and initialize the ConfigParser

--- src/main/kotlin/com/github/darkvanityoflight/darkmodcore/DarkModCore.kt
+++ src/main/kotlin/com/github/darkvanityoflight/darkmodcore/DarkModCore.kt
@@ -1,4 +1,13 @@
 package com.github.darkvanityoflight.darkmodcore
 
-class DarkModCore {
+import com.github.darkvanityoflight.darkmodcore.configparser.ADarkModConfigParser
+import com.github.darkvanityoflight.darkmodcore.configparser.ConfigParser
+
+class DarkModCore : ADarkMod() {
+    override var configParser: ADarkModConfigParser = ConfigParser()
+
+    override fun onEnable() {
+        super.onEnable()
+        info("[DarkModCore] Enabling DarkModCore")
+    }
 }
