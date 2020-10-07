79b2494f56bd7792b9a159430ec789ec94bca80d
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-07 14:52:22 +0200

    chore: Init ConfigParser

---
+++ src/main/kotlin/com/github/darkvanityoflight/darkmodcore/configparser/ConfigParser.kt
@@ -0,0 +1,7 @@
+package com.github.darkvanityoflight.darkmodcore.configparser
+
+class ConfigParser : ADarkModConfigParser() {
+    override fun read() {
+        TODO("Not yet implemented")
+    }
+}
