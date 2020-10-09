5b0fdef3fcc19c39a196c17714a03123a213d0d0
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-09 13:50:01 +0200

    feat: PersistentDataType StringList

---
+++ src/main/kotlin/com/github/darkvanityoflight/darkmodcore/tagutils/persistentdatatypes/StringList.kt
@@ -0,0 +1,58 @@
+package com.github.darkvanityoflight.darkmodcore.tagutils.persistentdatatypes
+
+import org.bukkit.persistence.PersistentDataAdapterContext
+import org.bukkit.persistence.PersistentDataType
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
+import java.util.*
+
+
+class StringArrayItemTagType : PersistentDataType<ByteArray, Array<String>> {
+	private val charset : Charset
+
+	constructor(charset: Charset){
+		this.charset = charset
+	}
+
+	constructor(){
+		charset  = Charset.defaultCharset()
+	}
+
+	override fun getPrimitiveType(): Class<ByteArray> {
+		return ByteArray::class.java
+	}
+
+	override fun getComplexType(): Class<Array<String>> {
+		return Array<String>::class.java
+	}
+
+	override fun toPrimitive(strings: Array<String>, itemTagAdapterContext: PersistentDataAdapterContext): ByteArray {
+		val allStringBytes = arrayOfNulls<ByteArray>(strings.size)
+		var total = 0
+		for (i in allStringBytes.indices) {
+			val bytes = strings[i].toByteArray(charset)
+			allStringBytes[i] = bytes
+			total += bytes.size
+		}
+		val buffer: ByteBuffer = ByteBuffer.allocate(total + allStringBytes.size * 4) //stores integers
+		for (bytes in allStringBytes) {
+			buffer.putInt(bytes!!.size)
+			buffer.put(bytes)
+		}
+		return buffer.array()
+	}
+
+	override fun fromPrimitive(bytes: ByteArray, itemTagAdapterContext: PersistentDataAdapterContext): Array<String> {
+		val buffer: ByteBuffer = ByteBuffer.wrap(bytes)
+		val list = ArrayList<String>()
+		while (buffer.remaining() > 0) {
+			if (buffer.remaining() < 4) break
+			val stringLength: Int = buffer.int
+			if (buffer.remaining() < stringLength) break
+			val stringBytes = ByteArray(stringLength)
+			buffer.get(stringBytes)
+			list.add(String(stringBytes, charset))
+		}
+		return list.toTypedArray()
+	}
+}
