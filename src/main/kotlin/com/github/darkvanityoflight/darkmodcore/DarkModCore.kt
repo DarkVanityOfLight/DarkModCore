e462583167d6d7dcbb722cefb3d6145d8ac452a7
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-14 15:56:18 +0200

    feat: parse the FileConfiguration to the ConfigParser

--- src/main/kotlin/com/github/darkvanityoflight/darkmodcore/DarkModCore.kt
+++ src/main/kotlin/com/github/darkvanityoflight/darkmodcore/DarkModCore.kt
@@ -3,7 +3,7 @@ package com.github.darkvanityoflight.darkmodcore
 import com.github.darkvanityoflight.darkmodcore.configparser.ConfigParser
 
 class DarkModCore : ADarkMod() {
-    var configParser: ConfigParser = ConfigParser()
+    var configParser: ConfigParser = ConfigParser(config)
 
     override fun onEnable() {
         super.onEnable()
--- src/main/kotlin/com/github/darkvanityoflight/darkmodcore/configparser/ConfigParser.kt
+++ src/main/kotlin/com/github/darkvanityoflight/darkmodcore/configparser/ConfigParser.kt
@@ -1,6 +1,8 @@
 package com.github.darkvanityoflight.darkmodcore.configparser
 
-class ConfigParser : ADarkModConfigParser() {
+import org.bukkit.configuration.file.FileConfiguration
+
+class ConfigParser(config: FileConfiguration) : ADarkModConfigParser(config) {
     override fun read() {
         TODO("Not yet implemented")
     }
