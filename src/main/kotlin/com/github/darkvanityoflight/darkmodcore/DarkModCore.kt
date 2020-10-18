ed82d5f538ed10d6c2497b474e688b7ac0b4b14c
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-18 16:15:16 +0200

    feat: register commands

--- src/main/kotlin/com/github/darkvanityoflight/darkmodcore/DarkModCore.kt
+++ src/main/kotlin/com/github/darkvanityoflight/darkmodcore/DarkModCore.kt
@@ -1,5 +1,7 @@
 package com.github.darkvanityoflight.darkmodcore
 
+import com.github.darkvanityoflight.darkmodcore.commands.PluginLoader
+import com.github.darkvanityoflight.darkmodcore.commands.PluginUnloader
 import com.github.darkvanityoflight.darkmodcore.configparser.ConfigParser
 
 class DarkModCore : ADarkMod() {
@@ -8,5 +10,8 @@ class DarkModCore : ADarkMod() {
     override fun onEnable() {
         super.onEnable()
         info("[DarkModCore] Enabling DarkModCore")
+
+        this.getCommand("load")?.setExecutor(PluginLoader(this))
+        this.getCommand("unload")?.setExecutor(PluginUnloader(this))
     }
 }
