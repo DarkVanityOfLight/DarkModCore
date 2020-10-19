b4c9752242793a2b8753298d368adeb3d802756c
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-19 17:22:01 +0200

    refractor: ADarkModConfig Parser to ARecraftedConfigParser

--- src/main/kotlin/com/github/darkvanityoflight/recraftedcore/configparser/ARecraftedConfigParser.kt
+++ src/main/kotlin/com/github/darkvanityoflight/recraftedcore/configparser/ARecraftedConfigParser.kt
@@ -2,5 +2,5 @@ package com.github.darkvanityoflight.recraftedcore.configparser
 
 import org.bukkit.configuration.file.FileConfiguration
 
-abstract class ADarkModConfigParser(val config: FileConfiguration) : IParse {
+abstract class ARecraftedConfigParser(val config: FileConfiguration) : IParse {
 }
--- src/main/kotlin/com/github/darkvanityoflight/recraftedcore/configparser/ConfigParser.kt
+++ src/main/kotlin/com/github/darkvanityoflight/recraftedcore/configparser/ConfigParser.kt
@@ -2,7 +2,7 @@ package com.github.darkvanityoflight.recraftedcore.configparser
 
 import org.bukkit.configuration.file.FileConfiguration
 
-class ConfigParser(config: FileConfiguration) : ADarkModConfigParser(config) {
+class ConfigParser(config: FileConfiguration) : ARecraftedConfigParser(config) {
     override fun read() {
         TODO("Not yet implemented")
     }
