251bdd550e3ada7ca1284aaff8b7245b4b6ffc2e
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-02-16 18:40:38 +0100

    chore: Dummy test for the CI

---
+++ src/main/kotlin/com/github/darkvanityoflight/recraftedcore/DummyTest.kt
@@ -0,0 +1,12 @@
+package com.github.darkvanityoflight.recraftedcore
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+
+internal class DummyTest{
+
+    @Test
+    fun dummy(){
+
+    }
+}
