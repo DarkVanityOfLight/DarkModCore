6c044663b309e6b0464eb12a7ad8e17297273994
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-02-18 10:47:25 +0100

    fix: Make the close button working

--- src/main/kotlin/com/github/darkvanityoflight/recraftedcore/gui/elements/CloseButton.kt
+++ src/main/kotlin/com/github/darkvanityoflight/recraftedcore/gui/elements/CloseButton.kt
@@ -2,10 +2,29 @@ package com.github.darkvanityoflight.recraftedcore.gui.elements
 
 import com.github.darkvanityoflight.recraftedcore.gui.Clickable
 import com.github.darkvanityoflight.recraftedcore.gui.GUIManager
+import com.github.darkvanityoflight.recraftedcore.utils.itemutils.setLore
+import com.github.darkvanityoflight.recraftedcore.utils.itemutils.setName
+import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
+object CloseButtonFactory{
+    private val closeItemStack = ItemStack(Material.BARRIER)
+    private val closeButton: CloseButton
+    init{
+        closeItemStack.setName("Close")
+        closeItemStack.setLore("Close this inventory")
+
+        closeButton = CloseButton(closeItemStack)
+    }
+
+    fun getCloseButton(): CloseButton{
+        return closeButton
+    }
+}
+
 class CloseButton(itemStack: ItemStack) : Clickable(itemStack) {
+
     override fun onClick(player: Player) {
         GUIManager.forceClose(player)
     }
