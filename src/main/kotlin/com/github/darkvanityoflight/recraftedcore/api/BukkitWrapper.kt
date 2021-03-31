9d5f05e5fdcd3a9cca938b5b8b4d3f1d5d0926a4
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-03-31 15:08:33 +0200

    feat: Add wrapping for getPluginManager

--- src/main/kotlin/com/github/darkvanityoflight/recraftedcore/api/BukkitWrapper.kt
+++ src/main/kotlin/com/github/darkvanityoflight/recraftedcore/api/BukkitWrapper.kt
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryType
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.InventoryHolder
+import org.bukkit.plugin.PluginManager
 import java.util.*
 import java.util.logging.Level
 
@@ -79,5 +80,9 @@ open class BukkitWrapper{
 
     open fun getOnlinePlayers(): Set<Player>{
         return Bukkit.getOnlinePlayers().toSet()
+    }
+
+    open fun getPluginManager(): PluginManager{
+        return Bukkit.getPluginManager()
     }
 }
